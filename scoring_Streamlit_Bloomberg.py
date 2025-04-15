2b10cf29feeee397c7058daf33dad57eb2271e1b
francescaarese <66136048+francescaarese@users.noreply.github.com>
2025-04-15 16:19:05 +0100

    Update scoring_Streamlit_Bloomberg.py

--- scoring_Streamlit_Bloomberg.py
+++ scoring_Streamlit_Bloomberg.py
@@ -51,9 +51,9 @@ weights = {
     # 'Recent Financing Score': st.sidebar.slider("Recent Financing Score Weight", 0.0, 1.0, 0.1, 0.01),
     'Company Growth Score': st.sidebar.slider("Company Growth Score Weight", 0.0, 1.0, 0.20, 0.01),
     'Emerging and Verticals Score': st.sidebar.slider("Emerging and Verticals Score Weight", 0.0, 1.0, 0.1, 0.01),
-  # 'Innovation ChatGPT Score': st.sidebar.slider("Innovation ChatGPT Score Weight", 0.0, 1.0, 0.15, 0.01),
-   # 'Media Outreach ChatGPT Score': st.sidebar.slider("Media Outreach ChatGPT Score Weight", 0.0, 1.0, 0.15, 0.01),
- # 'Judges Score': st.sidebar.slider("Judges Score Weight", 0.0, 1.0, 0.05, 0.01),
+   'Innovation ChatGPT Score': st.sidebar.slider("Innovation ChatGPT Score Weight", 0.0, 1.0, 0.15, 0.01),
+    'Media Outreach ChatGPT Score': st.sidebar.slider("Media Outreach ChatGPT Score Weight", 0.0, 1.0, 0.15, 0.01),
+  'Judges Score': st.sidebar.slider("Judges Score Weight", 0.0, 1.0, 0.05, 0.01),
 }
 
 
